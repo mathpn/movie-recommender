a7888e873846f1d5a1f28c31ea2cac8e30dcc8ea
Matheus Pedroni <pnmatheus@protonmail.com>
2022-10-11 22:11:39 -0300

    insert ratings and users in DB also

--- scripts/insert_data_into_db.py
+++ scripts/insert_data_into_db.py
@@ -5,16 +5,18 @@ Will be included in Docker compose. For now, initialize postgres Docker with:
     docker run --name postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=movies -p 5401:5432 -d postgres
 """
 import asyncio
-import json
-from ast import literal_eval
 import math
+from ast import literal_eval
+from datetime import datetime
+from functools import partial
 
-import pandas as pd
-import numpy as np
 import asyncpg
+import pandas as pd
 
-from app.db.postgres import insert_movie_metadata
-from app.models import MovieMetadata
+from app.db.postgres import (create_metadata_table, create_ratings_table,
+                             create_users_table, insert_movie_metadata,
+                             insert_movie_rating, insert_user)
+from app.models import MovieMetadata, Rating
 
 POSTGRES_URI = "postgresql://postgres:postgres@localhost:5401/movies"
 
@@ -45,6 +47,15 @@ def is_integer(field):
         return False
 
 
+def get_tmdb_id(rating_id, links) -> int:
+    values = links[links["movieId"] == rating_id]["tmdbId"].values
+    if len(values) == 0:
+        return -1
+    elif math.isnan(values[0]):
+        return -1
+    return int(values[0])
+
+
 async def main():
     movies_metadata = pd.read_csv("./data/movies_metadata.csv")
     movie_credits = pd.read_csv("./data/credits.csv")
@@ -66,10 +77,11 @@ async def main():
         movies_metadata[feature] = movies_metadata[feature].apply(get_list)
 
     pool = await asyncpg.create_pool(POSTGRES_URI)
+    await create_metadata_table(pool)
     row_dicts = movies_metadata.to_dict(orient="records")
     tasks = []
     for row in row_dicts:
-        if not isinstance(row['title'], str) and math.isnan(row['title']):
+        if not isinstance(row["title"], str) and math.isnan(row["title"]):
             continue
         metadata = MovieMetadata(
             row["id"],
@@ -83,6 +95,31 @@ async def main():
             row["vote_count"],
         )
         tasks.append(asyncio.create_task(insert_movie_metadata(pool, metadata)))
+    await asyncio.gather(*tasks)
+
+    ratings = pd.read_csv("./data/ratings_small.csv")
+    links = pd.read_csv("./data/links.csv")
+    get_ids = partial(get_tmdb_id, links=links)
+    ratings["tmdb_id"] = ratings["movieId"].apply(get_ids)
+    await create_ratings_table(pool)
+    tasks = []
+    for row_dict in ratings.to_dict(orient="records"):
+        if row_dict["tmdb_id"] < 0:
+            continue
+        rating = Rating(
+            user_id=row_dict["userId"],
+            movie_id=row_dict["tmdb_id"],
+            rating=int(row_dict["rating"] * 10),
+            timestamp=datetime.fromtimestamp(row_dict["timestamp"]),
+        )
+        tasks.append(asyncio.create_task(insert_movie_rating(pool, rating)))
+    await asyncio.gather(*tasks)
+
+    await create_users_table(pool)
+    tasks = []
+    max_user_id = ratings['userId'].max()
+    for i in range(max_user_id):
+        tasks.append(asyncio.create_task(insert_user(pool, f"data_{i + 1}")))
     await asyncio.gather(*tasks)
 
 
