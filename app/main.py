45679ce6dd5476d625bfa83466e2367b3f5e24b9
Matheus Pedroni <pnmatheus@protonmail.com>
2022-10-12 17:56:33 -0300

    initial implementation of recommend endpoint

--- app/main.py
+++ app/main.py
@@ -1,21 +1,32 @@
 import os
+from time import perf_counter
 
 import asyncpg
 from fastapi import FastAPI, Query, Request
 from starlette.responses import JSONResponse
 
-from app.db.postgres import insert_user, get_all_movies_genres
-from app.lookup import create_genre_searcher
+from app.db.postgres import (get_all_movies_genres,
+                             get_keyword_searcher_fields, insert_user)
+from app.logger import logger
+from app.lookup import (GenreSearcher, KeywordSearcher, create_genre_searcher,
+                        create_keyword_searcher)
+
+# from fastapi_cprofile.profiler import CProfileMiddleware
+
 
 app = FastAPI()
+# app.add_middleware(CProfileMiddleware, enable=True, print_each_request=True, strip_dirs=False, sort_by='tottime')
 
 
 @app.on_event("startup")
 async def startup_event():
     app.state.pool = await asyncpg.create_pool(os.environ["POSTGRES_URI"])
-    movie_ids, genres = await get_all_movies_genres(app.state.pool)
-    app.state.genre_searcher = create_genre_searcher(movie_ids, genres)
-
+    movies_genres = await get_all_movies_genres(app.state.pool)
+    app.state.genre_searcher = create_genre_searcher(
+        movies_genres["movie_ids"], movies_genres["genres"]
+    )
+    keyword_fields = await get_keyword_searcher_fields(app.state.pool)
+    app.state.keyword_searcher = create_keyword_searcher(keyword_fields)
 
 
 @app.post("/create_user")
@@ -24,17 +35,28 @@ async def create_user(request: Request, username: str) -> JSONResponse:
         await insert_user(request.app.state.pool, username)
         return JSONResponse(f"created user {username}")
     except asyncpg.UniqueViolationError:
-        # TODO insert logging
-        return JSONResponse(f"user {username} already exists", status_code=400)
+        msg = f"user {username} already exists"
+        logger.error(msg)
+        return JSONResponse(msg, status_code=400)
     except Exception as exc:
-        # TODO logging
-        return JSONResponse("create user crashed", status_code=500)
+        msg = "create user crashed"
+        logger.error(msg + " " + str(exc))
+        return JSONResponse(msg, status_code=500)
 
 
 @app.get("/recommend")
 async def recommend(request: Request, username: str, movie_id: int) -> JSONResponse:
-    # TODO
-    raise NotImplementedError()
+    init = perf_counter()
+    genre_searcher: GenreSearcher = request.app.state.genre_searcher
+    keyword_searcher: KeywordSearcher = request.app.state.keyword_searcher
+    recos_by_genre = genre_searcher.search_by_movie(movie_id=movie_id, k=2000)
+    logger.info(len(recos_by_genre))
+    recos_by_kw, _ = keyword_searcher.search_by_movie(
+        movie_id=movie_id, k=5, allowed_movie_ids=recos_by_genre
+    )
+    end = perf_counter()
+    logger.info(f"{(end - init) * 1000:.2f} ms")
+    return JSONResponse(recos_by_kw)
 
 
 @app.post("/rate")
