e14e446164c5c4e3393495b6e818810b5b3c0a8d
Matheus Pedroni <pnmatheus@protonmail.com>
2022-10-12 20:45:28 -0300

    test: update keyword searcher tests with new behavior

--- tests/test_keyword_searcher.py
+++ tests/test_keyword_searcher.py
@@ -2,43 +2,35 @@ from app.lookup import create_keyword_searcher
 from app.models import KeywordFields
 
 
+MOVIE_1 = KeywordFields(1, ["action"], ["foo", "bar"], ["ping", "pong"], "foobar")
+MOVIE_2 = KeywordFields(2, ["drama"], ["f#o", "b@r"], ["ping", "pong"], "f#ob@r")
+MOVIE_3 = KeywordFields(3, ["adventure"], ["something", "else"], ["pang", "pung"], "foobar")
+
+
 def test_keyword_searcher_match_order():
-    movie_1 = KeywordFields(1, ["foo", "bar"], ["ping", "pong"], "foobar")
-    movie_2 = KeywordFields(2, ["f#o", "b@r"], ["ping", "pong"], "f#ob@r")
-    movie_3 = KeywordFields(3, ["something", "else"], ["pang", "pung"], "foobar")
-    searcher = create_keyword_searcher([movie_1, movie_2, movie_3])
+    searcher = create_keyword_searcher([MOVIE_1, MOVIE_2, MOVIE_3])
     result = searcher.search_by_movie(2, k=3)
-    movie_idx, scores = zip(*result)
-    assert movie_idx == (2, 1, 3)
-    assert scores[0] - 1.0 < 1e-4
+    movie_idx, scores = result
+    assert movie_idx == [1, 3]
     assert sorted(scores, reverse=True) == list(scores)
 
 
 def test_keyword_searcher_k():
-    movie_1 = KeywordFields(1, ["foo", "bar"], ["ping", "pong"], "foobar")
-    movie_2 = KeywordFields(2, ["f#o", "b@r"], ["ping", "pong"], "f#ob@r")
-    movie_3 = KeywordFields(3, ["something", "else"], ["pang", "pung"], "foobar")
-    searcher = create_keyword_searcher([movie_1, movie_2, movie_3])
+    searcher = create_keyword_searcher([MOVIE_1, MOVIE_2, MOVIE_3])
     result = searcher.search_by_movie(2, k=2)
-    movie_idx, _ = zip(*result)
-    assert movie_idx == (2, 1)
+    movie_idx, _ = result
+    assert movie_idx == [1, 3]
 
 
 def test_keyword_searcher_allowed_movies():
-    movie_1 = KeywordFields(1, ["foo", "bar"], ["ping", "pong"], "foobar")
-    movie_2 = KeywordFields(2, ["f#o", "b@r"], ["ping", "pong"], "f#ob@r")
-    movie_3 = KeywordFields(3, ["something", "else"], ["pang", "pung"], "foobar")
-    searcher = create_keyword_searcher([movie_1, movie_2, movie_3])
+    searcher = create_keyword_searcher([MOVIE_1, MOVIE_2, MOVIE_3])
     result = searcher.search_by_movie(1, k=3, allowed_movie_ids=[2, 3])
-    movie_idx, _ = zip(*result)
-    assert movie_idx == (2, 3)
+    movie_idx, _ = result
+    assert movie_idx == [3]
 
 
 def test_keyword_searcher_large_k():
-    movie_1 = KeywordFields(1, ["foo", "bar"], ["ping", "pong"], "foobar")
-    movie_2 = KeywordFields(2, ["f#o", "b@r"], ["ping", "pong"], "f#ob@r")
-    movie_3 = KeywordFields(3, ["something", "else"], ["pang", "pung"], "foobar")
-    searcher = create_keyword_searcher([movie_1, movie_2, movie_3])
+    searcher = create_keyword_searcher([MOVIE_1, MOVIE_2, MOVIE_3])
     result = searcher.search_by_movie(1, k=100)
-    movie_idx, _ = zip(*result)
-    assert movie_idx == (1, 2, 3)
+    movie_idx, _ = result
+    assert movie_idx == [2, 3]
