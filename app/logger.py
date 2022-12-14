da829b3eeaaa89f3a06e9b838a3e79cbdbd23b36
Matheus Pedroni <pnmatheus@protonmail.com>
2022-12-13 22:09:24 -0300

    fix log level settings

--- app/logger.py
+++ app/logger.py
@@ -2,37 +2,36 @@ import logging
 import os
 from logging.config import dictConfig
 
-from pydantic import BaseModel
+
+LOGGER_NAME = "movie_recommender"
+LOG_FORMAT = "%(levelprefix)s | %(asctime)s | %(message)s"
 
 
-class LogConfig(BaseModel):
-    """Logging configuration to be set for the server"""
-
-    LOGGER_NAME: str = "movie_recommender"
-    LOG_FORMAT: str = "%(levelprefix)s | %(asctime)s | %(message)s"
-    LOG_LEVEL: str = "DEBUG"
-
-    # Logging config
-    version = 1
-    disable_existing_loggers = False
-    formatters = {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": LOG_FORMAT,
-            "datefmt": "%Y-%m-%d %H:%M:%S",
+def create_log_config(log_level: str):
+    """Logging configuration."""
+    return {
+        "logger_name": LOGGER_NAME,
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": LOG_FORMAT,
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
+        },
+        "handlers": {
+            "default": {
+                "formatter": "default",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stderr",
+            },
+        },
+        "loggers": {
+            "movie_recommender": {"handlers": ["default"], "level": log_level},
         },
     }
-    handlers = {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-    }
-    loggers = {
-        "movie_recommender": {"handlers": ["default"], "level": LOG_LEVEL},
-    }
 
 
-dictConfig(LogConfig(LOG_LEVEL="DEBUG" if os.environ.get("DEBUG") else "WARNING").dict())
+dictConfig(create_log_config(log_level="DEBUG" if os.environ.get("DEBUG") else "INFO"))
 logger = logging.getLogger("movie_recommender")
